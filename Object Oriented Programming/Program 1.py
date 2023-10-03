7fbeb1ee1c163545fe8be6a5572020f1d832c39c
Indrajeet Mondal <131744577+unrivalledhitman120703@users.noreply.github.com>
2023-10-03 19:21:39 +0530

    Basic overview of OOP

---
+++ Object Oriented Programming/Program 1.py
@@ -0,0 +1,31 @@
+# Introduction to OOPs
+# Name = Indrajeet Mondal; Date = 24th October 2023
+# SourceCode
+
+# Some introductory examples:
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('Hi'))
+print(type([]))
+print(type(()))
+print(type({}))
+
+# Output
+# <class 'NoneType'>
+# <class 'bool'>
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'list'>
+# <class 'tuple'>
+# <class 'dict'>
+
+# Inference
+# Here, we can see that all the types are "class" + "respective date-types"
+
+# Definition
+# A class is a blueprint for creating objects. It is a user-defined data type that encapsulates data and
+# functions that operate on that data. A class can be thought of as a template for creating objects, which
+# are instances of the class.
