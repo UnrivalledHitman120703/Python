fccd1925d0b632e2be0a31bfd2a97da27f95f1f1
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-04 14:14:47 +0530

    Created a class and instantiated it

---
+++ Object Oriented Programming/Program 2.py
@@ -0,0 +1,32 @@
+# Creating my own classes and objects
+# Name = Indrajeet Mondal; Date = 4th October 2023
+# SourceCode
+
+# Creating a PlayerCharacter class
+class PlayerCharacter:
+    # __init__ is a constructor, it is called everytime an object of the class is created
+    def __init__(self, name, age):
+        # self.name and self.age are attributes
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print(f'{self.name} is running !!')
+        return 'done'
+
+
+# Instantiating the PlayerCharacter class/ Creating an object of the PlayerCharacter class
+player1 = PlayerCharacter('Skullduggery', 250)
+player2 = PlayerCharacter('Lord Vile', 100)
+
+# Displaying class attributes
+print(player1.name, player1.age)
+print(player2.name, player2.age)
+
+# Calling class methods
+print(player1.run())
+print(player2.run())
+
+# Both the objects are at different memory locations
+print(player1)
+print(player2)
