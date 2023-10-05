12b84ac8fd5f138cc5661b79146f6a8bf2f54799
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-05 12:57:41 +0530

    Learnt the use of Dunder methods

---
+++ Object Oriented Programming/Program 9.py
@@ -0,0 +1,16 @@
+# Dunder methods or magic functions examples
+# Name = Indrajeet Mondal; Date = 5th October 2023
+# SourceCode
+
+class SuperList(list):
+    def __len__(self):
+        return 1000
+
+
+super_list1 = SuperList()
+
+print(len(super_list1))
+super_list1.append(5)
+print(super_list1[0])
+print(issubclass(SuperList, list))
+print(issubclass(list, object))
