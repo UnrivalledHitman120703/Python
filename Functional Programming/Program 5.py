4cf3e59f15b62f0e1aab2cc0b73c41b99b231fd7
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-06 11:14:54 +0530

    Added 2 examples of lambda expression

--- Functional Programming/Program 5.py
+++ Functional Programming/Program 5.py
@@ -17,3 +17,16 @@ print(list(filter(lambda item: item % 2 != 0, list1)))
 print('Reduce with lambda:- ')
 print(list1)
 print(reduce(lambda acc, item: acc + item, list1))
+
+# Some examples of lambda expression implementation
+
+# Example 1: Square
+list1 = [5, 4, 3]
+
+new_list = list(map(lambda num: num ** 2, list1))
+print(new_list)
+
+# Example 2: List Sorting
+a = [(0, 2), (4, 3), (10, -1), (9, 9)]
+a.sort(key = lambda x: x[1])
+print(a)
