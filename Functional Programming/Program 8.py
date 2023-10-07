a2c3957b76447d92120e2ae53cbbffd3f3167892
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-07 10:35:38 +0530

    List Comprehensions extra example

---
+++ Functional Programming/Program 8.py
@@ -0,0 +1,8 @@
+# Practice on Comprehensions
+# Name = Indrajeet Mondal; Date = 6th October 2023
+# SourceCode
+
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+duplicates = list(set([x
+                       for x in some_list if some_list.count(x) > 1]))
+print(duplicates)
