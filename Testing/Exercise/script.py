4a638c59dac7534e82b862c8f5a5bd8cf60a6bce
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-14 15:27:07 +0530

    Completed the unittest module

--- Testing/Exercise/script.py
+++ Testing/Exercise/script.py
@@ -4,15 +4,24 @@
 
 import random
 
-answer = random.randint(1,10)
-while True:
-    try:
-        guess = int(input("Guess a number between 1 and 10:- "))
-        if 0 < guess < 11:
-            if(guess == answer):
-                print("You are a genius !!")
-                break
 
-    except ValueError:
-        print("Please enter a number !!")
-        continue
+def run_guess(guess, answer):
+    if 0 < guess < 11:
+        if guess == answer:
+            print(answer and "You are a genius!")
+            return True
+    else:
+        print("hey bozo, I said 1~10")
+
+
+answer = random.randint(1, 10)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            guess = int(input("Guess a number 1~10: "))
+            if run_guess(guess, answer):
+                break
+        except ValueError:
+            print("Please enter a number")
+            continue
---
+++ Testing/Exercise/test.py
@@ -0,0 +1,28 @@
+# Writing test cases for script.py
+# Name = Indrajeet Mondal; Date = 14th October 2023
+# SourceCode
+
+import unittest
+import script
+
+
+class TestGame(unittest.TestCase):
+    def test_input(self):
+        result = script.run_guess(5, 5)
+        self.assertTrue(result)
+
+    def test_input_wrong_guess(self):
+        result = script.run_guess(5, 0)
+        self.assertFalse(result)
+
+    def test_input_wrong_number(self):
+        result = script.run_guess(5, 11)
+        self.assertFalse(result)
+
+    def test_input_wrong_type(self):
+        result = script.run_guess(5, '11')
+        self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
