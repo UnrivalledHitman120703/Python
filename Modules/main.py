d77c4595e3824c46f324dc12c19435a6e8f0be23
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-15 12:31:54 +0530

    Learnt about Packages

--- Modules/main.py
+++ Modules/main.py
@@ -2,7 +2,7 @@
 # Name = Indrajeet Mondal; Date = 15th October 2023
 # SourceCode
 
-
+import shopping.shopping_cart as cart
 import utility
 
 print(utility)
@@ -10,3 +10,6 @@ print(utility)
 print("Using utility.divide() present in utility.py:- ", utility.divide(8, 4))
 
 print("Using utility.multiply() present in utility.py:- ", utility.multiply(2, 4))
+
+print("Using add_item() from shopping.shopping_cart: ")
+cart.add_item("Apple")
---
+++ Modules/shopping/shopping_cart.py
@@ -0,0 +1,7 @@
+# Adding shopping items to cart
+# Name = Indrajeet Mondal; Date = 15th October 2023
+# SourceCode
+
+
+def add_item(item):
+    print(f"{item} added to cart !!")
