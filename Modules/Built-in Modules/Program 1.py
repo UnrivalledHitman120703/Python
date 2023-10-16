99e88ff57a9e1e38a9fb105dc25d31355b11e139
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-16 22:36:12 +0530

    Random Module intro

---
+++ Modules/Built-in Modules/Program 1.py
@@ -0,0 +1,9 @@
+# Learning about python built-in modules
+# Name = Indrajeet Mondal; Date = 16th October 2023
+# SourceCode
+
+import random
+
+print("randint from random; range provided is (1, 10):- ",random.randint(1,10))
+print("choice from random; list passed: [1,2,3,4,5]", random.choice([1,2,3,4,5]))
+print("shuffle from random; list passed: [1,2,3]", random.shuffle([1,2,3]))
