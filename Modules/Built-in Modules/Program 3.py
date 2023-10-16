757b8ed0b447d49c9a08258d00e1ef8c801401e8
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-17 01:04:12 +0530

    Used PyJokes

---
+++ Modules/Built-in Modules/Program 3.py
@@ -0,0 +1,8 @@
+# Using the pyjokes module (pip install)
+# Name = Indrajeet Mondal; Date = 17th October 2023
+# SourceCode
+
+import pyjokes
+
+joke = pyjokes.get_joke('en', 'neutral')
+print(joke)
